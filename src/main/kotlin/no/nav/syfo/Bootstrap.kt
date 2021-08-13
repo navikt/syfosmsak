6180c0edb38770ee2585a744607716b4d2399ad7
andreasnilsen <danduras@gmail.com>
2021-08-13 12:56:26 +0200

    update timeout for testing

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -241,7 +241,7 @@ suspend fun blockingApplicationLogic(
                     msgId = receivedSykmelding.msgId,
                     sykmeldingId = receivedSykmelding.sykmelding.id
             )
-            withTimeout(Duration.ofSeconds(30)) {
+            withTimeout(Duration.ofMillis(10)) {
                 journalService.onJournalRequest(receivedSykmelding, validationResult, loggingMeta)
             }
         }
