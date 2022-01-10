ec298aa8fea625d51b6be3845f81eed9294c413c
andreasnilsen <danduras@gmail.com>
2022-01-10 08:57:39 +0100

    update plugins

--- syfosmsak-stream/build.gradle.kts
+++ syfosmsak-stream/build.gradle.kts
@@ -4,5 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     java
+    id("org.jmailen.kotlinter") version "3.6.0"
+    id("com.diffplug.spotless") version "5.16.0"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
