226448c6a47a54ee1f1eb358a18e6cb42c2290cf
Tia Firing <tia.firing@nav.no>
2022-01-21 10:22:08 +0100

    test for vedlegg

--- syfosmsak-app/src/test/kotlin/no/nav/syfo/client/DokArkivClientKtTest.kt
+++ syfosmsak-app/src/test/kotlin/no/nav/syfo/client/DokArkivClientKtTest.kt
@@ -8,21 +8,25 @@ import org.amshove.kluent.shouldNotBeEqualTo
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
-class DokArkivClientKtTest : Spek ({
+class DokArkivClientKtTest : Spek({
     describe("VedleggToPdf") {
         it("Returnerer samme vedlegg hvis vedlegget er PDF") {
             val vedleggMessage: VedleggMessage = objectMapper.readValue(DokArkivClientTest::class.java.getResourceAsStream("/vedlegg_pdf.json"))
+            val gosysVedlegg = toGosysVedlegg(vedleggMessage.vedlegg)
 
-            val oppdatertVedlegg = vedleggToPDF(vedleggMessage.vedlegg)
+            val oppdatertVedlegg = vedleggToPDF(gosysVedlegg)
 
-            oppdatertVedlegg shouldBeEqualTo vedleggMessage.vedlegg
+            oppdatertVedlegg shouldBeEqualTo gosysVedlegg
         }
         it("Konverterer til PDF hvis vedlegget ikke er PDF") {
             val vedleggMessage: VedleggMessage = objectMapper.readValue(DokArkivClientTest::class.java.getResourceAsStream("/vedlegg_bilde.json"))
+            val gosysVedlegg = toGosysVedlegg(vedleggMessage.vedlegg)
 
-            val oppdatertVedlegg = vedleggToPDF(vedleggMessage.vedlegg)
+            val oppdatertVedlegg = vedleggToPDF(gosysVedlegg)
 
-            oppdatertVedlegg shouldNotBeEqualTo vedleggMessage.vedlegg
+            oppdatertVedlegg shouldNotBeEqualTo gosysVedlegg
+            oppdatertVedlegg.contentType shouldBeEqualTo "application/pdf"
+            oppdatertVedlegg.description shouldBeEqualTo vedleggMessage.vedlegg.description
         }
     }
 })
