8f9854352c368fd7e922c1628fad68f8e8db1129
Joakim Taule Kartveit <joakimkartveit@gmail.com>
2023-02-16 12:04:28 +0100

    Moved alerts to sykmelding-alerts repo

--- syfosmsak-app/src/main/kotlin/no/nav/syfo/Environment.kt
+++ syfosmsak-app/src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,9 +1,5 @@
 package no.nav.syfo
 
-import java.nio.charset.StandardCharsets
-import java.nio.file.Files
-import java.nio.file.Paths
-
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationName: String = getEnvVar("NAIS_APP_NAME", "syfosmsak"),
@@ -22,5 +18,3 @@ data class Environment(
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
     System.getenv(varName) ?: defaultValue ?: throw RuntimeException("Missing required variable \"$varName\"")
-
-fun getFileAsString(filePath: String) = String(Files.readAllBytes(Paths.get(filePath)), StandardCharsets.UTF_8)
