129cc35d5bd649c6fa503bafe17df2eeb3713faa
Helene Arnesen <helene.arnesen@nav.no>
2024-04-04 11:04:25 +0200

    fix: padstart hprnummerSignerer

--- src/main/kotlin/no/nav/syfo/client/DokArkivClient.kt
+++ src/main/kotlin/no/nav/syfo/client/DokArkivClient.kt
@@ -129,6 +129,9 @@ fun hprnummerMedRiktigLengdeOgFormat(
         return hprnummerKunTallBehandler.padStart(9, '0')
     }
     if (hprnummerKunTallBehandler.length == 9) return hprnummerKunTallBehandler
+    if (hprnummerKunTallSignerer.length < 9) {
+        return hprnummerKunTallSignerer.padStart(9, '0')
+    }
     return hprnummerKunTallSignerer
 }
 
